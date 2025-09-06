77ac920160726d59bbb0fb078718a6ed8c2458d5
Shawn <shrimpwagon@gmail.com>
2025-09-06 15:44:37 -0400

    Fix shell quoting in funky name test
    FIXED: Shell argument parsing issue with special characters
    
    Problem:
    - Single quotes inside double quotes were being stripped
    - Caused spaces in display name/description to split arguments
    - Example: --display-name 'My Super Project' became multiple args
    
    Debug showed:
    --display-name My Süper Åwesome Prøject! --description This has spëcial chars: @#$%&*()
    
    Solution:
    - Changed outer quotes from double to single quotes
    - Changed inner quotes from single to double quotes
    - Proper shell quoting: 'podium new ... --display-name "text with spaces"'
    
    Result: Special characters and spaces in display names/descriptions
    will now be properly passed as single arguments to the CLI.

--- src/scripts/test_json_output.sh
+++ src/scripts/test_json_output.sh
@@ -285,7 +285,7 @@ run_json_test "new_php7" \
 
 # Test 13: New Project - Funky Name and Description  
 run_json_test "new_funky_name" \
-    "podium new funky-special-test --framework laravel --display-name 'My Süper Åwesome Prøject!' --description 'This has spëcial chars: @#\$%&*()' --emoji '🦄' --json-output --debug" \
+    'podium new funky-special-test --framework laravel --display-name "My Süper Åwesome Prøject!" --description "This has spëcial chars: @#$%&*()" --emoji "🦄" --json-output --debug' \
     "Create project with special characters in display name and description"
 
 # Test 14: Setup Blank Folder (should work)
